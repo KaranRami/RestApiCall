05d16437d8dc6f9507e5ad527117ca3777085ab5
Karan Rami <32388574+KaranRami@users.noreply.github.com>
2019-04-19 23:53:47 +0530

    changes

--- ApiUtils/ApiUtils/Constants.cs
+++ ApiUtils/ApiUtils/Constants.cs
@@ -6,7 +6,7 @@ namespace ApiUtils
 {
     public static class Constants
     {
-        public static string BaseUrl = "https://staging.site-owl.com/siteowl/";
-        public static string LoginUrl = "public/api/login";
+        public static string BaseUrl = "";
+        public static string LoginUrl = "";
     }
 }
